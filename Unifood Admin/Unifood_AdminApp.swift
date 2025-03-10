fbbd0451df2c048f318a2343d6b74c951f4dd2f3
Ilia <ilia.loviagin@gmail.com>
2025-03-10 16:01:40 +0300

    Initial Commit

---
+++ Unifood Admin/Unifood_AdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unifood_AdminApp.swift
+//  Unifood Admin
+//
+//  Created by Ilia Loviagin on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Unifood_AdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
